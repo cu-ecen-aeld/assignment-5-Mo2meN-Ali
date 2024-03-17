43a068421f3b2984bd783853791a6d811f7ec73e
Mo2meN-Ali <momen.embedded@gmail.com>
2024-03-17 23:44:07 +0200

    Add solution assignment-7-part-2

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,1 +1,2 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_PROJECT_BASE_PATH)/package/aesd-assignments/aesd-assignments.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_PROJECT_BASE_PATH)/package/ldd/ldd.mk))
